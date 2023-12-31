ce62db4e9faa4325c98bd4962d6a382d0a2c94d9
Tim Supan <timsupann@gmail.com>
2023-12-30 18:33:36 -0800

    Create event listeners for the two buttons and console.log to confirm functionality

--- src/index.js
+++ src/index.js
@@ -8,3 +8,23 @@
 */
 
 import './style.css';
+
+//IIFE which adds event listeners to both buttons
+(function (){
+    const categoryBtn = document.querySelector('#categoryBtn');
+    categoryBtn.addEventListener('click', () =>{
+        console.log('categoryBtn event listener works!');
+    });
+
+    const todoBtn = document.querySelector('#todoBtn');
+    todoBtn.addEventListener('click', () => {
+        console.log('todoBtn event listener works!');
+    });
+})();
+
+//function createCategory(){}
+
+//function createToDo(){}
+
+
+
