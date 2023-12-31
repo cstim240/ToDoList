e05debc4c36c8084c22aed9fc9e205d8c0813e95
Tim Supan <timsupann@gmail.com>
2023-12-30 17:42:42 -0800

    Add brainstorm notes and divs for categories and items in html

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//import statements (other js files, photos, )
+/*
+ to do brainstorm 
+
+ each individual todo item should have a: title, description, dueDate, priority
+
+ todo items are contained in todo folders
+
+
+*/
